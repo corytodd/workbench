9d26b0c93bd9ceb8984710e124b23dd1644464af
Cory Todd <cory@corytodd.us>
2026-06-07 12:05:00 -0700

    cmake: switch to cpm

--- cmake/setup_benchmark.cmake
+++ cmake/setup_benchmark.cmake
@@ -1,18 +1,20 @@
-set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
-set(INSTALL_GTEST OFF)
-set(BUILD_GTEST ON)
-set(BUILD_GMOCK ON)
+include(CPM)
 
-# Dependencies
-include(FetchContent)
-FetchContent_Declare(
-        benchmark
-        GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG eddb0241389718a23a42db6af5f0164b6e0139af # v1.9.4
+CPMAddPackage(
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    GIT_TAG v1.17.0
+    OPTIONS
+        "INSTALL_GTEST OFF"
+        "BUILD_GMOCK ON"
 )
-FetchContent_Declare(
-        googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG 52eb8108c5bdec04579160ae17225d66034bd723 # v1.17.0
+
+CPMAddPackage(
+    NAME benchmark
+    GITHUB_REPOSITORY google/benchmark
+    GIT_TAG v1.9.4
+    OPTIONS
+        "BENCHMARK_ENABLE_TESTING OFF"
+        "BENCHMARK_ENABLE_INSTALL OFF"
+        "BENCHMARK_ENABLE_GTEST_TESTS OFF"
 )
-FetchContent_MakeAvailable(benchmark googletest)
