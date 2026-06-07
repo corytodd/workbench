d1255507f64bb1950d0d408ff3dce0a7c15ee728
Cory Todd <cory@corytodd.us>
2026-06-07 12:46:46 -0700

    cmake: remove unused google test  package install

--- cmake/setup_benchmark.cmake
+++ cmake/setup_benchmark.cmake
@@ -1,15 +1,6 @@
 include(CPM)
 
 CPMAddPackage(
-    NAME googletest
-    GITHUB_REPOSITORY google/googletest
-    GIT_TAG v1.17.0
-    OPTIONS
-        "INSTALL_GTEST OFF"
-        "BUILD_GMOCK ON"
-)
-
-CPMAddPackage(
     NAME benchmark
     GITHUB_REPOSITORY google/benchmark
     GIT_TAG v1.9.5
